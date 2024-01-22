238edaf47d27d24099ffc7431f32e931fa1c496b
Muizniek <muiznieka@gmail.com>
2024-01-22 11:40:19 +0200

    Added real-time chat window.

--- src/views/Chatroom.vue
+++ src/views/Chatroom.vue
@@ -1,18 +1,28 @@
 <template>
-<div class="container">
-    <Navbar/>
-</div>
+  <div class="container">
+    <Navbar />
+    <ChatWindow />
+    <NewChatForm />
+  </div>
 </template>
 
-<script >
-import Navbar from '@/components/Navbar.vue';
+<script>
+import ChatWindow from "@/components/ChatWindow.vue";
+import Navbar from "@/components/Navbar.vue";
+import NewChatForm from "@/components/NewChatForm.vue";
+import { projectAuth } from "@/firebase/config";
+import router from "@/router";
 
 export default {
-    components: {Navbar}
-}
-
+  components: { Navbar, NewChatForm, ChatWindow },
+  setup() {
+    projectAuth.onAuthStateChanged((user) => {
+      if (!user) {
+        router.push({ name: "Welcome" });
+      }
+    });
+  },
+};
 </script>
 
-<style>
-
-</style>
+<style></style>
